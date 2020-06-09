e1b7043c6a589d79b45cf097059e9d5c4dad6d4d
Madhawa <madhawa@outlook.com>
2020-06-09 23:18:03 +0530

    traveller details

--- cypress/integration/crv_sprint_six.js
+++ cypress/integration/crv_sprint_six.js
@@ -1,21 +1,23 @@
 describe ('visit_url', function() {
-    it('visit_howserv_product', function() {
+
+    beforeEach(function() {
+        Cypress.Cookies.preserveOnce('JSESSIONID')
+        //Cypress.Cookies.debug(true)
+    })
+
+
+     it('visit_howserv_product', function() {
         cy.visit('https://qa09sts.intertrav.co.uk/travelinsurance/quote/policy-details')
         //cy.url().should('include','Travel Insurance Quote |Over 50s holiday insurance |Staysure')
     })
 
     it('trip parameters', function() {
-        cy.get('#cover > div > div:nth-child(3) > label').click()
+        cy.get('#cover > .question-container > :nth-child(2) > label').click()
         cy.get('#going-cruise > div > div:nth-child(3) > label').click()
+        cy.get('#multiple-destination > .question-container > :nth-child(3)')
     })
 
-    it('select country', function() {
-        cy.get('#toLocationAnnual').select('EUROPE_LR')
-    })
 
-    it('dates', function() {
-        cy.get('input[id=datepicker-departure-text]').should('be.visible').should('be.enabled').type("14/06/2020")
-    })
 
     it('cover for', function() {
         cy.get('#cover-for > div > div:nth-child(2) > label').click()
@@ -26,19 +28,26 @@ describe ('visit_url', function() {
         cy.get('input[id=lastname]').should('be.visible').should('be.enabled').type("Ratnayake")
         cy.get('input[id=email]').should('be.visible').should('be.enabled').type("madhawa@intervest.lk")
         cy.get('input[id=dayTimeTelephone]').should('be.visible').should('be.enabled').type("0771257025")
-        cy.get('input[id=postcode]').should('be.visible').should('be.enabled').type("NN47YB")
+        cy.get('input[id=postcode]').should('be.visible').should('be.enabled').type("NN47YB")        
+    })
+
+    it('dates', function() {
+        cy.get('input[id=datepicker-departure-text]').should('be.visible').should('be.enabled').type("14/06/2020")
+              
+    })
+
+    it('select country', function() {
+        cy.get('#countrySearchInput').type("France")
+        cy.get('#countrySearchInput').type('{downarrow}{enter}')
+        cy.get('#multiple-destination > .question-container > :nth-child(3)')
+        
     })
 
     it('submit travller details', function() {
+        cy.get('input[id=datepicker-return-text]').should('be.visible').should('be.enabled').type("30/06/2020")  
         cy.get('input[id=btnSubmit]').click()
-    })
+    })    
 
-    it('fucking session', function() {
-        cy.wait(2000)
-        cy.get('.sessionBtn')
-    })
-
-  
 
 
 })
