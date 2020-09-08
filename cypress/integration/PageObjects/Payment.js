66678576b00682354167ff5c60982492bdef4422
Madhawa Ratnayake <mdmadhawaratnayake@gmail.com>
2020-09-08 16:21:39 +0530

    Adopted changes to payment gateway smartpay

--- cypress/integration/PageObjects/Payment.js
+++ cypress/integration/PageObjects/Payment.js
@@ -1,23 +1,33 @@
 class Payment {
 
     cardNumber() {
-        return cy.get('input[id="card.cardNumber"]')
+        // return cy.get('input[id="card.cardNumber"]')
+        return cy.get('#cardNumber')
     }
     cardHolderName() {
-        return cy.get('input[id="card.cardHolderName"]')
+        //return cy.get('input[id="card.cardHolderName"]')
+        return cy.get('#cardholderName')
     }
     expiryMonth() {
-        return cy.get('select[id="card.expiryMonth"]')
+        // return cy.get('select[id="card.expiryMonth"]')
+        return cy.get('#expiryMonth')
     }
     expiryYear() {
-        return cy.get('select[id="card.expiryYear"]')
+        // return cy.get('select[id="card.expiryYear"]')
+        return cy.get('#expiryYear')
     }
     cvcCode() {
-        return cy.get('input[id="card.cvcCode"]')
+        // return cy.get('input[id="card.cvcCode"]')
+        return cy.get('#csc')
     }
 
     submit() {
-        return cy.get('input[name=pay]')
+        // return cy.get('input[name=pay]')
+        return cy.get('#btnSubmit')
+    }
+
+    back() {
+        
     }
 }
 
